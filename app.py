5ba36d628d9d0c2a41af08b01a13fea8a211791a
cewarkentin <101836882+cewarkentin@users.noreply.github.com>
2022-06-14 18:42:48 -0500

    9.5.2 setting up the flask weather app

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-# import flask dependency
-from flask import Flask
+# Set Up Flask Weather App
 
-# create a new flask app instance
+#import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Set Up the Database
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+# Set Up Flask
 app = Flask(__name__)
 
-# create flask routes
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+# Welcome route
+@app.route("/")
+def welcome():
+    return(
+        '''
+        Welcome to the Climate Analysis API!
+        Available Routes:
+        /api/v1.0/precipitation
+        /api/v1.0/stations
+        /api/v1.0/tobs
+        /api/v1.0/temp/start/end
+        ''')
